31a5de2a8b3e9fe459ca17aae7cca1ce8af3f39a
Lithi Mgwebi <lithim11@gmail.com>
2024-08-28 10:51:25 +0200

    Fixing errors with document upload

--- client/src/components/Requisition/Dialogs/MotivationDialog.vue
+++ client/src/components/Requisition/Dialogs/MotivationDialog.vue
@@ -1,39 +1,43 @@
 <template>
-	<v-card max-width="400" prepend-icon="mdi-update">
-		<div>
-			<v-data-table-server :headers="headers" :items="motivations">
-				<template
-					v-slot:[`item.delete`]="{ item }"
-					v-if="
-						user.id == requisition.applicant.id &&
-						requisition.managerRecommendation == null
-					"
-				>
-					<v-btn v-on:click="deleteMotivation(item)">Delete</v-btn>
-				</template>
-			</v-data-table-server>
-		</div>
-		<div
-			v-if="user.id == requisition.applicant.id && requisition.managerRecommendation == null"
-		>
-			<section class="create">
-				<h3>Upload Motivation</h3>
-				<form @submit.prevent="saveMotivation" enctype="multipart/form-data">
-					<input
-						type="file"
-						accept="application/pdf"
-						ref="file"
-						@change="(e) => (file = e.target.files[0])"
-						required
-					/>
-					<button type="submit">Upload</button>
-				</form>
-			</section>
-		</div>
-		<template v-slot:actions>
-			<v-btn class="ms-auto" text="Close" @click="closeDialog"></v-btn>
-		</template>
-	</v-card>
+	<div>
+		<v-card max-width="400" prepend-icon="mdi-update">
+			<div>
+				<v-data-table-server :headers="headers" :items="motivations">
+					<template
+						v-slot:[`item.delete`]="{ item }"
+						v-if="
+							user.id == requisition.applicant.id &&
+							requisition.managerRecommendation == null
+						"
+					>
+						<v-btn v-on:click="deleteMotivation(item)">Delete</v-btn>
+					</template>
+				</v-data-table-server>
+			</div>
+			<div
+				v-if="
+					user.id == requisition.applicant.id && requisition.managerRecommendation == null
+				"
+			>
+				<section class="create">
+					<h3>Upload Motivation</h3>
+					<form @submit.prevent="saveMotivation" enctype="multipart/form-data">
+						<input
+							type="file"
+							accept="application/pdf"
+							ref="file"
+							@change="(e) => (file = e.target.files[0])"
+							required
+						/>
+						<button type="submit">Upload</button>
+					</form>
+				</section>
+			</div>
+			<template v-slot:actions>
+				<v-btn class="ms-auto" text="Close" @click="closeDialog"></v-btn>
+			</template>
+		</v-card>
+	</div>
 </template>
 
 <script setup>
@@ -56,7 +60,9 @@ const props = defineProps(['requisition'])
 const requisition = props.requisition
 
 watch(
-	motivations,
+	() => {
+		props.requisition
+	},
 	async (oldRequisition, newRequisition) => {
 		await getter(typeOfFile.Motivation, requisition.requisitionId)
 	},
